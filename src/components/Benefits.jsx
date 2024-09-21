8c6d45147d20877a30cebfc79e75729eaf04c9b8
mridul saggi <91930535+mridulsaggi@users.noreply.github.com>
2024-09-21 17:31:46 +0530

    added features section

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -53,7 +53,7 @@ const Benefits = () => {
         <img className='w-full md:w-1/2 p-4' src="https://a.storyblok.com/f/165154/1080x1080/6120ad9f16/helatoconnect-lp-hero-gif-updated_2-compressed.gif/m/" alt="" />
         <div className="w-[80%] mx-auto md:w-[30%] md:pl-8">
             <p className="text-gray-600 mb-2 text-2xl">For print producers</p>
-            <h2 className="text-3xl sm:text-5xl md:text-6xl font-bold mb-4">GelatoConnect</h2>
+            <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4">GelatoConnect</h2>
             <p className="text-[1rem] sm:text-xl my-6">
               Elevate efficiency, cut costs, and seamlessly automate with our all-in-one software solution for the print industry
             </p>
@@ -62,6 +62,21 @@ const Benefits = () => {
             </button>
           </div>
       </div>
+      <div className='flex bg-[#FAF6EF] flex-col p-24 items-center md:flex-row'>
+        
+        <div className="w-[80%] mx-auto md:w-[50%] md:pl-8">
+            <p className="text-gray-600 mb-2 text-2xl">For print on demand sellers</p>
+            <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4">Accelerate business growth with innovative design tools and apps</h2>
+            <p className="text-[1rem] sm:text-xl my-6">
+            See how our cutting-edge solutions can help you reach new customers and maximize your profits.
+            </p>
+            <button className="bg-black text-white py-2 px-6 rounded-full hover:bg-gray-800 transition">
+              Get Started
+            </button>
+          </div>
+          <img className='w-full md:w-1/2 p-4' src="https://a.storyblok.com/f/165154/1200x1200/35ff04c4c9/cc-55_marketing-assets-for-personalization-studio.gif/m/" alt="" />
+      </div>
+      
     </div>
   );
 };
